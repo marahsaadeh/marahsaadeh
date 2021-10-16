9ca69297f1db352aba244744b72caebc6ffa5309
marahsaadeh <92647123+marahsaadeh@users.noreply.github.com>
2021-10-16 22:09:52 +0300

    Create javas1.js

---
+++ javas1.js
@@ -0,0 +1,62 @@
+
+console.log("hello");
+var b = "hello";
+console.log(b);
+let age = 20;
+let name = "marah";
+let nname = {first:'adam',last:'ali'};
+let truth=false;
+let gro = ['apple','banana','oranges'];
+let random;
+let no=null;
+var c = 10;
+var C = 7;
+console.log(c+C);
+console.log(c*C);
+console.log(c-C);
+console.log(50/5);
+console.log(c%2);
+C++;
+console.log(C);
+C+=10;
+console.log(C);
+document.getElementById("some").innerHTML = "hello";
+var age = prompt("what is your age? ");
+document.getElementById("some").innerHTML = age;
+//Function1
+function fun(){
+ var name = prompt("what is your name?");
+var result="hello"+"  "+ name;
+console.log(result);
+}
+fun();
+//Function1
+function sum(c,C){
+var r=C+c;
+console.log(r);
+}
+sum(c,C);
+//While loops
+
+var x=0;
+while(x<50){
+x++;
+console.log(x);
+}
+//for loop
+for(let i=0;i<=100;i++){
+    console.log(i);
+}
+//Array
+let fruits = ['banana', 'apple', 'orange', 'pineapples'];
+fruits = new Array('banana', 'apple', 'orange', 'pineapples');
+console.log(fruits [2]); 
+ fruits [0] = 'pear'; 
+ console.log(fruits);
+ for (let i = 0; i < fruits. length; i++) { console. log(fruits [1]);
+     console.log('to string', fruits.toString());
+      console. Log(fruits.join(' * '));
+       console. Log(fruits.pop(), fruits); 
+       console. log (fruits.push('blackberries' ), fruits);
+ }
+
